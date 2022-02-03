78e93c00cac1ec13c125426e1652ec9b6355a680
Matheus Pereira <matttalves@gmail.com>
2022-02-03 17:32:00 -0300

    feat: test setup chartjs

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,14 @@
 <script>
     import TaskForm from './components/TaskForm.svelte';
     import ListTodos from './components/ListTodos.svelte';
+    import Canvas from './components/Canvas.svelte';
     export let name;
 </script>
 
 <main class="dark">
     <h1>{name}</h1>
 
+    <Canvas />
     <TaskForm />
 
     <ListTodos />
---
+++ src/components/Canvas.svelte
@@ -0,0 +1,41 @@
+<script>
+    import Chart from 'chart.js/auto';
+    import { onMount } from 'svelte';
+
+    let chartCanvas;
+
+    let chartData = [1, 1];
+    let chartInstance;
+
+    function addData(chart, data) {
+        chart.data.datasets[0].data = data;
+        chart.update();
+    }
+
+    function handler() {
+        addData(chartInstance, [4, 1]);
+    }
+
+    onMount(() => {
+        const chartConfig = {
+            type: 'pie',
+            data: {
+                labels: ['Completed', 'Todo'],
+                datasets: [
+                    {
+                        label: 'My First Dataset',
+                        data: chartData,
+                        backgroundColor: ['#00800062', '#c3000062'],
+                        hoverOffset: 4,
+                    },
+                ],
+            },
+            options: {},
+        };
+
+        chartInstance = new Chart(chartCanvas.getContext('2d'), chartConfig);
+    });
+</script>
+
+<canvas bind:this={chartCanvas} />
+<button on:click={handler}>click</button>
